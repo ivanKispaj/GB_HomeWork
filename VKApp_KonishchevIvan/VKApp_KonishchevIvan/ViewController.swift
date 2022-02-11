25d3afafbf6924059311a442737bd9ebd0166944
Konishchev Ivan <kispaj@mail.ru>
2022-02-11 08:48:11 +0300

    create repositiries VKApp KonishchevIvan

---
+++ VKApp_KonishchevIvan/VKApp_KonishchevIvan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKApp_KonishchevIvan
+//
+//  Created by Ivan Konishchev on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
