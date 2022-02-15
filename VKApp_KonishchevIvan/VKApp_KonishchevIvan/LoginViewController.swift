b330c443c735b59b897df3b2c072ea249b1029a4
Konishchev Ivan <kispaj@mail.ru>
2022-02-15 07:58:18 +0300

    Cerate Home Vork App lesson 2

--- VKApp_KonishchevIvan/VKApp_KonishchevIvan/LoginViewController.swift
+++ VKApp_KonishchevIvan/VKApp_KonishchevIvan/LoginViewController.swift
@@ -1,29 +1,73 @@
 //
-//  LoginViewController.swift
+//  ViewController.swift
 //  VKApp_KonishchevIvan
 //
-//  Created by Ivan Konishchev on 14.02.2022.
+//  Created by Ivan Konishchev on 11.02.2022.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
 
+
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBOutlet weak var passwordHelper: UILabel!
+   
+    @IBAction func enterButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailTextfield.addTarget(self, action: #selector(emailFieldDidChangeSelection(_:)), for: .editingChanged)
+        passwordTextfield.addTarget(self, action: #selector(passwordFieldDidChsngeSelection(_:)), for: .editingChanged)
+    }
+
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        let user =  UserDetail()
+        let emailInput = self.emailTextfield.text
+        let passwordInput = self.passwordTextfield.text
+        if user.useremail == emailInput && user.userPassword == passwordInput {
+           
+            return true
+        }
+        let allert = UIAlertController(title: "Error", message: "Введены неверные данные пользователя! Попробуйте снова.", preferredStyle: .alert)
+        let allertAction = UIAlertAction(title: "ОК", style: .cancel, handler: nil)
+        allert.addAction(allertAction)
+        present(allert, animated: true, completion: nil)
+       return false
+    }
+
+    
+}
+
+
+// MARK: - Verification textFields
+
+extension LoginViewController {
+    
+    @objc func passwordFieldDidChsngeSelection(_ textfield: UITextField){
+  
+        guard Validators.issimplePass( passwordTextfield.text! ) else {
+            self.passwordLabel.backgroundColor = .red
+            self.passwordHelper.backgroundColor = .red
+            self.passwordHelper.text = " Мин. 8 символов, заглавная буква и цифра! "
+            return
+        }
+        self.passwordLabel.backgroundColor = .white
+        self.passwordHelper.text = ""
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func emailFieldDidChangeSelection(_ textfield: UITextField){
+        guard Validators.isSimpleEmail(emailTextfield.text!) else {
+            self.emailLabel.backgroundColor = .red
+            return
+        }
+        self.emailLabel.backgroundColor = .white
     }
-    */
-
+    
 }
---
+++ VKApp_KonishchevIvan/VKApp_KonishchevIvan/TextFieldValidation/UserDetails.swift
@@ -0,0 +1,14 @@
+//
+//  UserDetails.swift
+//  VKApp_KonishchevIvan
+//
+//  Created by Ivan Konishchev on 14.02.2022.
+//
+
+import UIKit
+
+struct UserDetail {
+    let username = "Ivan Konishchev"
+    let useremail = "test@test.ru"
+    let userPassword = "qwerty1Test"
+}
