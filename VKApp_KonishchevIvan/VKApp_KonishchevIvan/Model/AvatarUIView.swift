0af430650fdf5494c5b42eaef9cc1e70444f01a8
Konishchev Ivan <kispaj@mail.ru>
2022-02-28 13:06:47 +0300

    Apgrade: edite avatar with interface builder

--- VKApp_KonishchevIvan/VKApp_KonishchevIvan/Model/AvatarUIView.swift
+++ VKApp_KonishchevIvan/VKApp_KonishchevIvan/Model/AvatarUIView.swift
@@ -7,20 +7,46 @@
 
 import UIKit
 
-class AvatarUIView: UIView {
+ class AvatarUIView: UIView {
 
     @IBOutlet weak var shadowViewCell: UIView!
     @IBOutlet weak var circleViewImage: UIImageView!
     
     
-    var shadowColor: CGColor = UIColor(named: "AvatarShadow")!.cgColor
+    // @IBInspectable var shadowColor: CGColor = UIColor(named: "AvatarShadow")!.cgColor
+     @IBInspectable var shadowColor: UIColor = .red
+     @IBInspectable var radius: CGFloat {
+         get {
+             return self.layer.cornerRadius
+         }
+         set {
+             self.layer.cornerRadius = newValue
+         }
+         
+     }
+     @IBInspectable var offset: CGSize  {
+         get {
+             return self.layer.shadowOffset
+         }
+         set{
+             self.layer.shadowOffset = newValue
+         }
+     }
+     @IBInspectable var shadowOpaciti: Float {
+         get {
+             return self.layer.shadowOpacity
+         }
+         set {
+             self.layer.shadowOpacity = newValue
+         }
+     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        shadowViewCell.layer.shadowColor = shadowColor
-        shadowViewCell.layer.shadowOffset = .zero
-        shadowViewCell.layer.shadowRadius = 7
-        shadowViewCell.layer.shadowOpacity = 0.7
+        shadowViewCell.layer.shadowColor = shadowColor.cgColor
+        shadowViewCell.layer.shadowOffset = offset
+        shadowViewCell.layer.shadowRadius = radius
+        shadowViewCell.layer.shadowOpacity = shadowOpaciti
     }
     override func layoutSubviews() {
         circleViewImage.layer.cornerRadius = bounds.height/2
